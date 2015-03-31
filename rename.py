fda59caef78dcdcddcf53bd3d7595ac03196455a
Jackson Wearn <jacksonwearn@gmail.com>
2015-03-31 15:51:33 -0400

    Update comments and for loop in def

--- rename.py
+++ rename.py
@@ -1,10 +1,17 @@
+# author: Jackson Wearn
+# description: a small script to rename the files in a directory
+# snippets taken from: http://stackoverflow.com/questions/225735/batch-renaming-of-files-in-a-directory
+
+
 import os
 
-def rename()
-    files = os.listdir('.') 
+files = os.listdir('.') 
+
+def rename( files ):
+
     for f in files:
 	print f
     return
 
-rename()
+rename(files)
 
