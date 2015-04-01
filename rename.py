abeb5cd1943e9ad4a7f7168073df854b400ca41b
Jackson Wearn <jacksonwearn@gmail.com>
2015-04-01 01:33:55 -0400

    Commented code, add case when user_path is '.'

--- rename.py
+++ rename.py
@@ -3,24 +3,32 @@
 # description: a small script to rename the files in a directory
 # initial idea from: http://stackoverflow.com/questions/225735/batch-renaming-of-files-in-a-directory
 
-import os
-import sys
-import re
+import os, sys, re
 
-args = sys.argv
+count = 0
+args = sys.argv # ['./rename.py', 'user_path/']
 
-if not len(args) == 2:
+if not len(args) == 2: # program exits if there is not exaclly one argument
     print "Input Error. Usage: rename.py <path>"
     exit(1)
 
-path = str(args[1:])
-path = path[2:len(path)-2]
+path = str(args[1:]) # returns string version of ['user_path/']
+path = path[2:len(path)-2] # strips [' and '] from user_path/
 
-files = os.listdir(path) 
+files = os.listdir(path) # returns a directory list of given user_path/
 
-for f in files:
+if path == '.':
+    path = '';
+
+for f in files: # loop through directory list (files)
+    # check that file doesn't begin with '.' and ends with 'avi','.mp4','.mkv','.srt' 
     if not f.startswith('.') and (f.endswith('.avi') or f.endswith('.mp4') or f.endswith('.mkv') or f.endswith('.srt')):
+	# check that file doesn't already follow the correct format [s[0-9]e[0-9]]
 	if not re.search('s[0-9]?[0-9]e[0-9]?[0-9]',f, re.IGNORECASE):
-	    idx_re = re.search('[0-9]?[0-9][0-9]',f).start()
-	    replacement = f[0:idx_re] + 's0' + f[idx_re] + 'e' + f[idx_re+1:]  
-	    os.rename(path + f, path + replacement)
+	    # index of first digit of 3 digit number (e.g. rome.hdtv.101.avi  : '101')
+	    idx_re = re.search('[0-9]?[0-9][0-9]',f).start() # this number determines what season the episode is in
+	    replacement = f[0:idx_re] + 's0' + f[idx_re] + 'e' + f[idx_re+1:]  # piece string together with correct format
+	    os.rename(path + f, path + replacement) # replace the original with the new file name
+	    count = count + 1 # increment to track how many files are updated
+
+print 'Finished: ' + str(count) + " file(s) updated"
